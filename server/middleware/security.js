7c56db0c6f892e2ae913fd7ca92b97b465f0d0d9
Patsifist <patricpaidla@gmail.com>
2025-04-17 16:01:26 +0300

    backend

--- server/middleware/security.js
+++ server/middleware/security.js
@@ -41,9 +41,9 @@ const configureSecurityMiddleware = (app) => {
   const corsOptions = {
     //only front domain in prod
     origin: process.env.NODE_ENV === 'production'
-      ? (process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['*'])
+      ? ['https://pilves.github.io', 'http://localhost:5173', '*']
       : ['http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     maxAge: 86400 // 24 hours
