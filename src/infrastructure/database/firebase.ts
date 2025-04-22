bc8bd44a51a430c26e3b2a5b4ebb763e7a37171d
Patsifist <patricpaidla@gmail.com>
2025-04-22 14:53:39 +0300

    ss

--- src/infrastructure/database/firebase.ts
+++ src/infrastructure/database/firebase.ts
@@ -13,6 +13,38 @@ export function initializeFirebase(): admin.firestore.Firestore {
     return firestoreInstance;
   }
 
+  try {
+    // set credentials path
+    let serviceAccount;
+
+    if (config.app.env == "production" && config.firebase.serviceAccount) {
+      serviceAccount = config.firebase.serviceAccount;
+      logger.info("Firebase service acocunt from ENV")
+    } else {
+
+      //local for dev
+      try {
+        serviceAccount = require("../../serviceAccountKey.json");
+        logger.info("Firebase service account from file");
+      } catch (error) {
+        logger.error("Failed to load serviceAccountKey.json", { error: (error as Error).message });
+        throw new Error("Firebase service account credentials not found. Please add service key");
+      }
+    }
+
+    // initialize firebase admin
+    admin.initializeApp({
+      credentials: admin.credential.cert(serviceAccount),
+    });
+
+    firestoreInstance = admin.firestore();
+
+
+
+
+  }
+
+
 }
 
 
